ab1130c31605bcc02bfe9ba77c2a72ed55848435
Arnaud Van Acker <arnaudvanacker@yahoo.fr>
2024-04-22 10:28:15 +0200

    navigate and events

---
+++ events/script.js
@@ -0,0 +1,76 @@
+let color,timer = 0;
+const body=document.querySelector('body');
+const log=document.querySelector('ul');
+const display=document.querySelector('.displayedsquare-wrapper');
+const squares=Array.from(document.querySelector('.actionsquare-wrapper').children);
+
+//square click
+squares.forEach(square => {
+    square.addEventListener('click', function() {
+        color=square.classList[1];
+        createSquare(color);
+        createLog('['+(timer/1000).toFixed(2)+'s] Created a new '+color+' square');
+        timer=0;
+    })
+});
+
+//delete square
+document.addEventListener('keyup', function(event) {
+    if (event.key === 's') {
+      const displayChildren = Array.from(display.children);
+      displayChildren.forEach(square => {
+        square.remove();
+      });
+    }
+});
+
+//change body color
+document.addEventListener('keyup', function(event) {
+    if (event.key === ' ') {
+      document.body.style.backgroundColor = randomColor();
+      createLog('['+(timer/1000).toFixed(2)+'s] Created a new background color');
+      timer=0;
+    }
+});
+
+//delete log
+document.addEventListener('keyup', function(event) {
+    if (event.key === 'l') {
+      const ulChildren = Array.from(document.querySelector('ul').children);
+      ulChildren.forEach(li => {
+        li.remove();
+      });
+    }
+});
+
+function createSquare(color) {
+    const newDiv=document.createElement('div');
+    newDiv.classList.add('displayedsquare',color);
+    display.appendChild(newDiv);
+    newDiv.addEventListener('click', function() {
+        alert(color);
+    })
+}
+
+function randomColor() {
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+    return '#'+randomColor;
+}
+
+function createLog(content) {
+    const newDiv=document.createElement('li');
+    newDiv.innerHTML=content;
+    log.appendChild(newDiv);
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function timerLoop() {
+    while(true) {
+        timer+=100;
+        await sleep(100);
+    }
+}
+timerLoop()
---
+++ navigate/script.js
@@ -0,0 +1,17 @@
+const ol=document.querySelector('ol');
+const olLastChild = document.querySelector('ol li:last-child');
+
+ol.insertBefore(olLastChild,ol.firstChild);
+
+
+
+const h2A=document.querySelectorAll('h2')[1];
+const h2B=document.querySelectorAll('h2')[2];
+
+const sectionA=document.querySelectorAll('section')[1];
+const sectionB=document.querySelectorAll('section')[2];
+
+sectionB.insertBefore(h2A, sectionB.firstChild);
+sectionA.insertBefore(h2B, sectionA.firstChild);
+
+sectionB.remove();
